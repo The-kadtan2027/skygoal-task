085e78fbdf363c0e90dc7264102c3a7013d52370
Gaju <gajanankadtan57@gmail.com>
2022-03-26 20:55:38 +0530

    minor changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,30 +1,27 @@
-
-import React from 'react'
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import MailOutlinedIcon from '@mui/icons-material/MailOutlined';
-import ShuffleOutlinedIcon from '@mui/icons-material/ShuffleOutlined';
-import "./styles.css"
+import React from "react";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import MailOutlinedIcon from "@mui/icons-material/MailOutlined";
+import ShuffleOutlinedIcon from "@mui/icons-material/ShuffleOutlined";
+import "./styles.css";
 
 function NavBar() {
   return (
-    <nav className='nav'>
-        <div className='nav-link'>
-
-            <p >Services</p>
-            <p >Product</p>
-            <p >Technology</p>
-            <p >About</p>
-            <p >Client</p>
-            <p >Partner</p>
+    <nav className="nav">
+      <div className="nav-link">
+        <p>Services</p>
+        <p>Product</p>
+        <p>Technology</p>
+        <p>About</p>
+        <p>Client</p>
+        <p>Partner</p>
         <div className="nav-contact">
-
-          <HomeOutlinedIcon />
-          <MailOutlinedIcon />
-          <ShuffleOutlinedIcon />
+          <HomeOutlinedIcon className="nav-icon" />
+          <MailOutlinedIcon className="nav-icon" />
+          <ShuffleOutlinedIcon className="nav-icon" />
         </div>
-        </div>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
